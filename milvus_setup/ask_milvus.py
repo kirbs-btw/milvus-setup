8e5f42122311e5a7cf58ca7011c15b7c81af0bc6
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-04-22 21:04:04 +0200

    Add files via upload

--- milvus_setup/ask_milvus.py
+++ milvus_setup/ask_milvus.py
@@ -22,24 +22,13 @@ def query_question(question, collection_milvus, model):
 
     result = collection_milvus.search(question_embedding, "embeddings", search_params, limit=5, output_fields=["content", "pagelabel"])
 
-
-    # getting the content from the search query 
-    """
-    for hits in result:
-        for hit in hits:
-            print(hit.entity.get("content"))
-            print(f"page: https://www.trumpf.com/filestorage/TRUMPF_US/Landingpages/TruLaser_2030_fiber/pdfs/TruLaser-2030-Pre-Install-Manual.pdf#page={hit.entity.get('pagelabel')}")
-            print("------------")
-
-    # make the question to a vector
-    """
     return result
 
 
 def search_db(question):
     # num_entities = 3000
     # dim = 768 # dim of the embeddings
-    collection_name = "test_new_bookv2"
+    collection_name = "test-name"
 
     # connecting to localhost db
     connections.connect("default", host="localhost", port="19530")
@@ -54,16 +43,19 @@ def search_db(question):
 
     results = query_question(question, collection_milvus, model)
 
-
-
     return results
 
+def main():
+    print("\n")
+    found = search_db("What is the Error Number 40300?")
 
+    for hits in found:
+        for hit in hits:
+            print("\n")
+            print(hit.entity.get("content"))
+            print(hit.entity.get("pagelabel"))
+    
+    
+if __name__ == '__main__':
+    main()
 
-print("\n")
-found = search_db("What is the Error Number 40300?")
-
-for hits in found:
-    for hit in hits:
-        print("\n")
-        print(hit.entity.get("content"))
